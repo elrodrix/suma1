d9ad4e0b41691501bbbc2679c773c7391683d020
Rodrigo Avila <rodrigo398@hotmail.com>
2015-04-08 15:51:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/rodrix/suma1/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.rodrix.suma1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText et1, et2;
+    private Button btnCalcular;
+    private TextView tvResultado;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
+        btnCalcular = (Button) findViewById(R.id.btnCalcular);
+        tvResultado = (TextView) findViewById(R.id.tvResultado);
+    }
+
+    private void onClickSuma(View view){
+        int a = Integer.valueOf(et1.getText().toString());
+        int b = Integer.valueOf(et2.getText().toString());
+        int c = a + b;
+
+        tvResultado.setText("La suma es: " + c);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
